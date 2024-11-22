bcb6f624617e1a7a9e245e9622e0e90c6ab08dd5
a1rzayev <asifrzayev2006@example.com>
2024-11-22 22:49:08 +0400

    Add validations for DTOs

--- AutoRepairManagerApp/src/AutoRepairManagerApp.Core/DTO/CarDTO.cs
+++ AutoRepairManagerApp/src/AutoRepairManagerApp.Core/DTO/CarDTO.cs
@@ -1,17 +1,19 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
-namespace AutoRepairManagerApp.Core.DTO
+namespace AutoRepairManagerApp.Core.DTO;
+public class CarDTO
 {
-    public class CarDTO
-    {
-        public string Make {  get; set; }
+    [Required(ErrorMessage = "Make is required.")]
+    [StringLength(20, ErrorMessage = "Make length cannot be more than 20 characters.")]
+    public string Make { get; set; }
 
-        public string Model { get; set; }
+    [Required(ErrorMessage = "Model is required.")]
+    [StringLength(20, ErrorMessage = "Model length cannot be more than 20 characters.")]
+    public string Model { get; set; }
 
-        public string CarPlate { get; set; }
-    }
+    [Required(ErrorMessage = "Car plate is required.")]
+    [RegularExpression(@"^\d{2}-\d{2}-\d{3}$", ErrorMessage = "Invalid car plate format. It should be in the format 00-AA-000.")]
+    [StringLength(7, MinimumLength = 7, ErrorMessage = "Car plate must be 7 characters!")]
+    public string CarPlate { get; set; }
 }
--- AutoRepairManagerApp/src/AutoRepairManagerApp.Core/Models/Car.cs
+++ AutoRepairManagerApp/src/AutoRepairManagerApp.Core/Models/Car.cs
@@ -11,8 +11,6 @@ public class Car
     [Required]
     public string? Model { get; set; }
     [Required]
-    [RegularExpression(@"^\d{2}-\d{2}-\d{3}$", ErrorMessage = "Invalid car plate format. It should be in the format 00-AA-000.")]
-    [StringLength(7, MinimumLength = 7, ErrorMessage = "Car plate must be 7 characters!")]
     public string? CarPlate { get; set; }
     [Required]
     public ICollection<RepairLog>? RepairHistory { get; set; }
