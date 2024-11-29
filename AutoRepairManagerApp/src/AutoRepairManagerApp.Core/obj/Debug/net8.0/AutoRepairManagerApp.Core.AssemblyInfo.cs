8ff6fb6b3ca0af64dcda7e1fc7cd3d1539ee85d8
bahtiarmirzoev <bakhagithub@gmail.com>
2024-11-29 08:16:25 -0800

    Mapper try to add

--- AutoRepairManagerApp/src/AutoRepairManagerApp.Core/obj/Debug/net8.0/AutoRepairManagerApp.Core.AssemblyInfo.cs
+++ AutoRepairManagerApp/src/AutoRepairManagerApp.Core/obj/Debug/net8.0/AutoRepairManagerApp.Core.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AutoRepairManagerApp.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d9c381ff5e3abea1195c071da7b1192f1f584dfb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bb0d5c6b241c18f48063a9336e329ad3d4c01f1e")]
 [assembly: System.Reflection.AssemblyProductAttribute("AutoRepairManagerApp.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AutoRepairManagerApp.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
